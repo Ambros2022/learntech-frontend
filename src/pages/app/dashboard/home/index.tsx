3e7602ea94bd9090633b26eb1ae26370a49e43f6
Your Name <aanchal10forever@gmail.com>
2024-04-06 10:59:58 +0530

    fix conflict error

---
+++ src/pages/app/dashboard/home/index.tsx
@@ -0,0 +1,37 @@
+// ** MUI Imports
+import Card from '@mui/material/Card'
+import Grid from '@mui/material/Grid'
+import Typography from '@mui/material/Typography'
+import CardHeader from '@mui/material/CardHeader'
+import CardContent from '@mui/material/CardContent'
+import axios1 from '../../../../configs/axios'
+import { ReactNode } from 'react'
+
+
+// ** Layout Import
+import BlankLayout from 'src/@core/layouts/BlankLayout'
+
+const Home = () => {
+  console.log(axios1, "main");
+  if (axios1.defaults.headers.common["x-access-token"]) {
+    console.log("x-access-token header is set:", axios1.defaults.headers.common["x-access-token"]);
+  } else {
+    console.log("x-access-token header is not set.");
+  }
+  return (
+    <Grid container spacing={6}>
+      <Grid item xs={12}>
+        <Card>
+          <CardContent>
+            <Typography sx={{ mb: 2 }}>Dashboard</Typography>
+          </CardContent>
+        </Card>
+      </Grid>
+
+    </Grid>
+  )
+}
+
+
+
+export default Home
