6471fa6e097b23b6b7a6d5a153ba1566ee491c01
abhishek <abhishekdahiya298@gmail.com>
2024-05-29 10:05:21 +0530

    home page header fixed

---
+++ src/@core/layouts/components/Header/abroad-dropdown.tsx
@@ -0,0 +1,58 @@
+import { useState, useCallback } from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+import React from 'react';
+
+const DropdownMenu = React.memo(({ states, type }: any) => {
+    const [visibleStates, setVisibleStates] = useState(10); // Initially show 10 states
+    const [isExpanded, setIsExpanded] = useState(false); // Track whether the list is expanded
+
+    const handleViewMore = useCallback(() => {
+        setVisibleStates(prev => Math.min(prev + 5, states.length));
+        setIsExpanded(true); // Set to expanded when viewing more
+    }, [states.length]);
+
+    const handleShowLess = useCallback(() => {
+        setVisibleStates(10); // Reset to initial number of visible states
+        setIsExpanded(false); // Set to collapsed
+    }, []);
+
+    return (
+        <>
+
+            {states.length > 0 && (
+                <ul className="dropdown-menu menu-icon" aria-labelledby="navbarDropdownMenuLink">
+                    <div className="text-center">
+                        <p style={{ fontWeight: 'bold', color: '#274896' }}>Best Country for Study</p>
+                    </div>
+                    {states.slice(0, visibleStates).map(item => (
+                        <li key={item.id}>
+                            <Link
+                                href={`/${item.slug}`}
+                                className="dropdown-item"
+                            >
+                                <div className="d-flex justify-content-between">
+                                {item?.country?.name}
+                                </div>
+                            </Link>
+                        </li>
+                    ))}
+                    <div className="text-center text-blue dropdownBtn">
+                        {visibleStates < states.length && (
+                            <button className="btn" onClick={handleViewMore}>
+                                View More
+                            </button>
+                        )}
+                        {isExpanded && (
+                            <button className="btn" onClick={handleShowLess}>
+                                Show Less
+                            </button>
+                        )}
+                    </div>
+                </ul>
+            )}
+        </>
+    );
+});
+
+export default DropdownMenu;
