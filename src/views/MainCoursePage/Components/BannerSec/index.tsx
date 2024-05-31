f737bda9916edb1a1b121db76ebda966450f0bfb
abhishek <abhishekdahiya298@gmail.com>
2024-05-31 16:36:18 +0530

    course page added

--- src/views/MainCoursePage/Components/BannerSec/index.tsx
+++ src/views/MainCoursePage/Components/BannerSec/index.tsx
@@ -1,7 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Link from 'next/link'
+import Autocomplete from 'src/@core/components/mui/autocomplete';
+import { useRouter } from 'next/router';
+import axios1 from 'axios';
+import axios from 'src/configs/axios';
+import { CircularProgress, IconButton, InputAdornment, TextField } from '@mui/material';
+import ClearIcon from '@mui/icons-material/Clear';
+import GlobalEnquiryForm from 'src/@core/components/popup/GlobalPopupEnquiry';
+let cancelToken: any;
 
-function BannerSec() {
+function BannerSec({ data }: any) {
+  const [searchResults, setSearchResults] = useState<any[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [open, setOpen] = useState(false);
+
+
+  const handleSearch = async (value) => {
+    if (value.length < 2) {
+      setSearchResults([]);
+      return;
+    }
+    try {
+      setLoading(true);
+      if (typeof cancelToken !== typeof undefined) {
+        cancelToken.cancel("Operation canceled due to new request.");
+      }
+      cancelToken = axios1.CancelToken.source();
+      const response = await axios.get('api/website/generalcourse/get', { cancelToken: cancelToken.token, params: { searchfrom: "name", searchtext: value } });
+      // const suggestions = response.data.data.flatMap((item: { data: any[]; type: any; }) => item.data.map(entry => ({
+      //   name: entry.name,
+      //   slug: entry.slug,
+      //   id: entry.id,
+      //   type: item.type,
+      // })));
+
+      if (response.status == 200) {
+        setSearchResults(response.data.data);
+      }
+
+      setOpen(true);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handleInputChange = (event, value) => {
+    handleSearch(value);
+  };
 
   return (
     <>
@@ -11,34 +58,90 @@ function BannerSec() {
           <div>
             <div className="row">
               <div className="col-md-7 mb-3">
-                <input type="search" className='form-control' placeholder='Search for course, degree or specialization' />
+                <Autocomplete
+                  open={open}
+                  onClose={() => setOpen(false)}
+                  onInputChange={handleInputChange}
+                  options={searchResults}
+                  getOptionLabel={(option) => option.name}
+                  renderOption={(props, option) => (
+                    <li {...props}>
+
+                      <Link href={`/course/${option.id}/${option?.streams?.slug}/${option.slug}`} style={{ color: "#000", textDecoration: 'none', display: 'block', width: '100%', height: '100%' }}>
+                        {option.name}
+                      </Link>
+
+
+                    </li>
+                  )}
+                  renderInput={(params) => (
+                    <TextField
+                      {...params}
+                      placeholder="Search for course, degree or specialization"
+                      className="form-control"
+                      InputProps={{
+                        ...params.InputProps,
+                        sx: {
+                          '& .MuiInputBase-input::placeholder': {
+                            color: 'black',
+                          },
+                        },
+                        endAdornment: (
+                          <React.Fragment>
+                            {loading ? <CircularProgress color="inherit" size={20} /> : null}
+                            {params.InputProps.endAdornment}
+                            {params.inputProps.value ? (
+                              <InputAdornment position="end">
+                                <IconButton
+                                  onClick={() => {
+                                    if (params.inputProps.onChange) {
+                                      const event = {
+                                        target: {
+                                          value: ''
+                                        }
+                                      } as React.ChangeEvent<HTMLInputElement>;
+                                      params.inputProps.onChange(event);
+                                    }
+                                  }}
+                                >
+                                  <ClearIcon />
+                                </IconButton>
+                              </InputAdornment>
+                            ) : null}
+                          </React.Fragment>
+                        ),
+                      }}
+                    />
+                  )}
+                />
+
+                {/* <input type="search" className='form-control' placeholder='Search for course, degree or specialization' /> */}
+
+
               </div>
-              <div className="mb-3 col-md-2 col-6 col-lg-1 col-xl-1 d-flex justify-content-start justify-content-md-center">
+              {/* <div className="mb-3 col-md-2 col-6 col-lg-1 col-xl-1 d-flex justify-content-start justify-content-md-center">
                 <button className='btn bg-white text-blue srchBtn'>Search</button>
-              </div>
+              </div> */}
             </div>
           </div>
           <h2 className='text-white fw-bold pt-3 mb-3'>Trending Courses</h2>
           <div className="d-flex pb-5 gap-2 flex-wrap justify-content-between">
             <div className='d-flex gap-2 flex-wrap mb-2 mb-lg-0'>
-              <a href="#" className='btn trendCrsBtn'>MBBS</a>
-              <a href="#" className='btn trendCrsBtn'>BDS</a>
-              <a href="#" className='btn trendCrsBtn'>MBA</a>
-              <a href="#" className='btn trendCrsBtn'>BBA</a>
-              <a href="#" className='btn trendCrsBtn'>BCA</a>
-              <a href="#" className='btn trendCrsBtn'>BSC</a>
-              <a href="#" className='btn trendCrsBtn'>BHMS</a>
-              <a href="#" className='btn trendCrsBtn'>BNYS</a>
+              {data.map((val) => (
+                <Link href={`/course/${val.id}/${val?.streams?.slug}/${val.slug}`}
+                  className='btn trendCrsBtn'>{val.short_name}</Link>
+              ))}
             </div>
             <div className='align-content-end'>
-              <button className='btn bg-warning text-white'>Check Eligibility</button>
+              <GlobalEnquiryForm buttonText="Check Eligibility" className="btn bg-warning text-white" />
+              {/* <button className='btn bg-warning text-white'>Check Eligibility</button> */}
             </div>
           </div>
         </div>
       </section>
       <div className='bg-white py-2'>
         <section className=" mt-2 container CourseNavigationLink">
-          <p><Link href={"/home"}>Home</Link>{'>'} <span className='text-blue'><Link className="text-blue" href={"/courses"}>Courses</Link></span></p>
+          <p><Link href={"/"}>Home</Link>{'>'} <span className='text-blue'>Courses</span></p>
         </section>
       </div>
     </>
--- src/views/MainCoursePage/Components/CoursesCard/index.tsx
+++ src/views/MainCoursePage/Components/CoursesCard/index.tsx
@@ -1,267 +1,93 @@
 import Image from 'next/image';
 import Link from 'next/link';
-import React, { useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
+import useIsMountedRef from 'src/hooks/useIsMountedRef';
+import axios from 'src/configs/axios';
 
 // The individual card component
 function CoursesCard({ course }) {
-
   return (
-    <>
-      <div id={course.id} className="col-md-4 col-lg-3">
-        <div className="card mb-3 p-2 bg-crsCard">
-          <div className="row g-0">
-            <div className="col-lg-4 coursesCardImg p-0 mb-3 mb-md-3 text-md-start text-center">
-              <Image width={200} height={200} src={course.imageUrl} className="rounded" alt="Courses-Card-img" />
-            </div>
-            <div className="col-lg-8 d-flex align-items-center ">
-              <div className="card-body p-0 ps-lg-2">
-                <h5 className="fw-bold text-blue card-title text-md-center text-center text-lg-start">{course.title}</h5>
-              </div>
-            </div>
+    <div id={course.id} className="col-md-4 col-lg-3">
+      <div className="card mb-3 p-2 bg-crsCard">
+        <div className="row g-0">
+          <div className="col-lg-3 p-0 mb-3 mb-md-0 text-md-start text-center">
+            <Image
+              width={50}
+              height={50}
+              src={`${process.env.NEXT_PUBLIC_IMG_URL}/${course.logo}`}
+              className="rounded"
+              alt="Courses-Card-img"
+            />
           </div>
-          <div className="row">
-            <div className="col-12 text-lg-start text-center mb-3">
-              <small className='fw-bold'>Streams</small>
-            </div>
-          </div>
-          <div>
-            <div className='min-vh-card'>
-              <div className="d-flex gap-2 mb-3 flex-wrap coursesBtn justify-content-md-start justify-content-center">
-                {course.streams.map(stream => (
-                  // eslint-disable-next-line react/jsx-key
-                  <a href="#" className='btn text-blue streamBtn'>{stream}</a>
-                ))}
-              </div>
-            </div>
-            <div className="d-grid pt-3">
-              <Link href={`/course/${course.id}/${course.slug}`} className='btn viewAllBtn'>View All {course.title} Courses</Link>
+          <div className="col-lg-8 d-flex align-items-center">
+            <div className="card-body p-0 ps-lg-2">
+              <h5 className="fw-bold text-blue card-title text-md-center text-center text-lg-start">{course.name}</h5>
             </div>
           </div>
         </div>
+        <div className="d-flex gap-2 my-0 my-md-3 mt flex-wrap coursesBtn justify-content-md-start justify-content-center min-vh-card">
+          {course.general_courses.map(val => (
+            <Link key={val.id} href={`/course/${val.id}/${course.slug}/${val.slug}`} className='btn text-blue streamBtn'>
+              {val.short_name}
+            </Link>
+          ))}
+        </div>
+        <div className="d-grid pt-1">
+          <Link href={`/course/${course.id}/${course.slug}`} className='btn viewAllBtn'>
+            View All {course.name} Courses
+          </Link>
+        </div>
       </div>
-    </>
+    </div>
   );
 }
 
 // Parent component handling the array
-function CoursesContainer() {
-  const [visibleCount, setVisibleCount] = useState(12); // Start by showing 12 cards
+function CoursesContainer({ data, Changesize }) {
+  const isMountedRef = useIsMountedRef();
+  const [streamcourses, setStreamcourses] = useState([]);
+  const [visibleCount, setVisibleCount] = useState(0);
+  const [datasize, setDatasize] = useState(12);
 
   const showMoreCourses = () => {
-    setVisibleCount(prevCount => prevCount + 8);
+    setDatasize(prevSize => prevSize + 8);
   };
 
-  const courses = [
-    {
-      id: 1,
-      title: "Medical",
-      slug: "Medical",
-      imageUrl: "/images/icons/filter-card.jpg",
-      streams: ["MBBS", "MD in Physiology"]
-    },
-    {
-      id: 2,
-      title: "Engineering",
-      slug: "Engineering",
-      imageUrl: "/images/icons/filter-card.jpg",
-      streams: ["Mechanical Engineering", "Computer Science"]
-    },
-    {
-      id: 3,
-      title: "Medical",
-      slug: "Medical",
-      imageUrl: "/images/icons/filter-card.jpg",
-      streams: ["MBBS", "MD in Physiology"]
-    },
-    {
-      id: 4,
-      title: "Engineering",
-      slug: "Engineering",
-      imageUrl: "/images/icons/filter-card.jpg",
-      streams: ["Mechanical Engineering", "Computer Science"]
-    },
-    {
-      id: 5,
-      title: "Medical",
-      slug: "Medical",
-      imageUrl: "/images/icons/filter-card.jpg",
-      streams: ["MBBS", "MD in Physiology"]
-    },
-    {
-      id: 6,
-      title: "Engineering",
-      slug: "Engineering",
-      imageUrl: "/images/icons/filter-card.jpg",
-      streams: ["Mechanical Engineering", "Computer Science"]
-    },
-    {
-      id: 7,
-      title: "Medical",
-      slug: "Medical",
-      imageUrl: "/images/icons/filter-card.jpg",
-      streams: ["MBBS", "MD in Physiology"]
-    },
-    {
-      id: 8,
-      title: "Engineering",
-      slug: "Engineering",
-      imageUrl: "/images/icons/filter-card.jpg",
-      streams: ["Mechanical Engineering", "Computer Science"]
-    },
-    {
-      id: 9,
-      title: "Medical",
-      slug: "Medical",
-      imageUrl: "/images/icons/filter-card.jpg",
-      streams: ["MBBS", "MD in Physiology"]
-    },
-    {
-      id: 10,
-      title: "Engineering",
-      slug: "Engineering",
-      imageUrl: "/images/icons/filter-card.jpg",
-      streams: ["Mechanical Engineering", "Computer Science"]
-    },
-    {
-      id: 11,
-      title: "Medical",
-      slug: "Medical",
-      imageUrl: "/images/icons/filter-card.jpg",
-      streams: ["MBBS", "MD in Physiology"]
-    },
-    {
-      id: 12,
-      title: "Engineering",
-      slug: "Engineering",
-      imageUrl: "/images/icons/filter-card.jpg",
-      streams: ["Mechanical Engineering", "Computer Science"]
-    },
-    {
-      id: 13,
-      title: "Medical",
-      slug: "Medical",
-      imageUrl: "/images/icons/filter-card.jpg",
-      streams: ["MBBS", "MD in Physiology"]
-    },
-    {
-      id: 14,
-      title: "Engineering",
-      slug: "Engineering",
-      imageUrl: "/images/icons/filter-card.jpg",
-      streams: ["Mechanical Engineering", "Computer Science"]
-    },
-    {
-      id: 15,
-      title: "Medical",
-      slug: "Medical",
-      imageUrl: "/images/icons/filter-card.jpg",
-      streams: ["MBBS", "MD in Physiology"]
-    },
-    {
-      id: 16,
-      title: "Engineering",
-      slug: "Engineering",
-      imageUrl: "/images/icons/filter-card.jpg",
-      streams: ["Mechanical Engineering", "Computer Science"]
-    },
-    {
-      id: 17,
-      title: "Medical",
-      slug: "Medical",
-      imageUrl: "/images/icons/filter-card.jpg",
-      streams: ["MBBS", "MD in Physiology"]
-    },
-    {
-      id: 18,
-      title: "Engineering",
-      slug: "Engineering",
-      imageUrl: "/images/icons/filter-card.jpg",
-      streams: ["Mechanical Engineering", "Computer Science"]
-    },
-    {
-      id: 19,
-      title: "Medical",
-      slug: "Medical",
-      imageUrl: "/images/icons/filter-card.jpg",
-      streams: ["MBBS", "MD in Physiology"]
-    },
-    {
-      id: 20,
-      title: "Engineering",
-      slug: "Engineering",
-      imageUrl: "/images/icons/filter-card.jpg",
-      streams: ["Mechanical Engineering", "Computer Science"]
-    },
-    {
-      id: 21,
-      title: "Medical",
-      slug: "Medical",
-      imageUrl: "/images/icons/filter-card.jpg",
-      streams: ["MBBS", "MD in Physiology"]
-    },
-    {
-      id: 22,
-      title: "Engineering",
-      slug: "Engineering",
-      imageUrl: "/images/icons/filter-card.jpg",
-      streams: ["Mechanical Engineering", "Computer Science"]
-    },
-    {
-      id: 23,
-      title: "Medical",
-      slug: "Medical",
-      imageUrl: "/images/icons/filter-card.jpg",
-      streams: ["MBBS", "MD in Physiology"]
-    },
-    {
-      id: 24,
-      title: "Engineering",
-      slug: "Engineering",
-      imageUrl: "/images/icons/filter-card.jpg",
-      streams: ["Mechanical Engineering", "Computer Science"]
-    },
-    {
-      id: 25,
-      title: "Medical",
-      slug: "Medical",
-      imageUrl: "/images/icons/filter-card.jpg",
-      streams: ["MBBS", "MD in Physiology"]
-    },
-    {
-      id: 26,
-      title: "Engineering",
-      slug: "Engineering",
-      imageUrl: "/images/icons/filter-card.jpg",
-      streams: ["Mechanical Engineering", "Computer Science"]
-    },
-    {
-      id: 27,
-      title: "Medical",
-      slug: "Medical",
-      imageUrl: "/images/icons/filter-card.jpg",
-      streams: ["MBBS", "MD in Physiology"]
-    },
-    {
-      id: 28,
-      title: "Engineering",
-      slug: "Engineering",
-      imageUrl: "/images/icons/filter-card.jpg",
-      streams: ["Mechanical Engineering", "Computer Science"]
-    },
-  ];
+  const getStreamwiseCourse = useCallback(async () => {
+    try {
+      const response = await axios.get('api/website/stream/general/get', {
+        params: {
+          page: 1,
+          size: datasize,
+        },
+      });
+      if (isMountedRef.current) {
+        setStreamcourses(response.data.data);
+        setVisibleCount(response.data.totalItems);
+      }
+    } catch (error) {
+      console.error('Failed to fetch trending courses:', error);
+    }
+  }, [datasize, isMountedRef]);
+
+  useEffect(() => {
+    getStreamwiseCourse();
+  }, [getStreamwiseCourse]);
 
   return (
     <section className="bg-white pt-5 pb-5">
       <div className="container">
         <div className="row">
-          {courses.slice(0, visibleCount).map((course, index) => (
-            <CoursesCard key={`${course.title}-${index}`} course={course} />
+          {streamcourses.map((course, index) => (
+            <CoursesCard key={index} course={course} />
           ))}
         </div>
-        {visibleCount < courses.length && ( // Only show the button if there are more courses to show
+        {visibleCount > streamcourses.length && (
           <div className="row">
             <div className="col-12 d-flex justify-content-center mb-3">
-              <button className='btn viewMoreBtnCourse' onClick={showMoreCourses}>View More</button>
+              <button className='btn viewMoreBtnCourse' onClick={showMoreCourses}>
+                View More
+              </button>
             </div>
           </div>
         )}
