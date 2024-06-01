2d0e5b4dc77af16d0567ffb0517d34fea1d16f5a
Ramanjot2325 <ramanjot2325@gamil.com>
2024-06-01 15:12:24 +0530

    ..

---
+++ src/views/CareerPage/Components/BannerSec/index.tsx
@@ -0,0 +1,35 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+
+const BannerSec = () => {
+    return (
+        <>
+            <section className='careerSec'>
+                <div className='h-100'>
+                    <div className="row h-100">
+                        <div className="col-md-6 bg-white py-md-0 py-5">
+                            <div className="d-flex justify-content-center h-100">
+                                <div className="align-content-center w-75">
+                                    <h1 className='fw-bold text-blue'>WE ARE HIRING</h1>
+                                    <h6 className='text-black mb-3'>Come join one of the Best Educational Service Provider Firms as rated by ISO 9001:2008 Certified.</h6>
+                                    <a className='btn currentOpeningBtn'>Current Openings</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-md-6 gx-0">
+                            <Image src='/images/icons/career-img.jpg' width={600} height={500} alt='career-img' />
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section className='bg-white'>
+                <div className="container py-3">
+                    <Link href='/' className='text-black'>Home</Link> {'>'} <Link className='text-blue' href='/career'>Career</Link>
+                </div>
+            </section>
+        </>
+    )
+}
+
+export default BannerSec
