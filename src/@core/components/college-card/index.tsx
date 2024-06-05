88edb04812fec30c0c46df3d80bf6cfbd1e30bae
Ramanjot2325 <ramanjot2325@gamil.com>
2024-06-05 13:01:27 +0530

    latest homepage card component

---
+++ src/@core/components/college-card/index.tsx
@@ -0,0 +1,28 @@
+// CollegeCard.js
+import React from 'react';
+import GlobalEnquiryForm from 'src/@core/components/popup/GlobalPopupEnquiry';
+import Link from 'next/link';
+import Image from 'next/image';
+
+function CollegeCard({ college }) {
+    return (
+        <div className="mx-xl-4 mx-lg-2 mx-md-2 mx-5 card featuredClgCard mb-4">
+            <div className='card-image'>
+                <Image src={`${process.env.NEXT_PUBLIC_IMG_URL}/${college.banner_image}`} width={200} height={200} className="card-Image-top" alt="featured-college" />
+            </div>
+            <div className="card-body">
+                <h5 className="card-title text-blue text-truncate">{college.name}</h5>
+                <p className="card-text text-truncate">
+                    <Image width={17} height={17} className="me-2 card-text-image" src="/images/icons/Location 2.svg" alt="location-icon" />
+                    {college.address}
+                </p>
+                <div className="d-flex justify-content-between">
+                    <GlobalEnquiryForm className="applyNowButton btn" />
+                    <Link href="/colleges" className="btn">View More</Link>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default CollegeCard; // Make sure to export the component
--- src/views/Homepage/Components/FeaturedCollegeSection/index.tsx
+++ src/views/Homepage/Components/FeaturedCollegeSection/index.tsx
@@ -4,6 +4,7 @@ import MainCarousel from 'src/@core/components/main-carousel'
 import axios1 from 'src/configs/axios'
 import GlobalEnquiryForm from 'src/@core/components/popup/GlobalPopupEnquiry';
 import Link from 'next/link';
+import CollegeCard from 'src/@core/components/college-card';
 
 function FeaturedCollegeSection() {
 
@@ -39,22 +40,11 @@ function FeaturedCollegeSection() {
       <div className="container pt-5 position-relative">
         <h2 className="fw-bold text-blue text-center mb-5">Featured Colleges</h2>
         <MainCarousel items={colleges.map(college => (
-          <div key={college.id} className="mx-xl-4 mx-lg-2 mx-md-2 mx-5 card featuredClgCard mb-4">
-            <Image src={`${process.env.NEXT_PUBLIC_IMG_URL}/${college.banner_image}`} width={300} height={300} className="card-Image-top" alt="featured-college" />
-            <div className="card-body">
-              <h5 className="card-title text-blue text-truncate">{college.name}</h5>
-              <p className="card-text text-truncate">
-                <Image width={17} height={17} className="me-2 card-text-image" src="/images/icons/Location 2.svg" alt="location-icon" />
-                {college.address}
-              </p>
-              <div className="d-flex justify-content-between">
-                {/* <a href="#" className="active btn">Apply Now</a> */}
-                <GlobalEnquiryForm className="applyNowButton btn" />
-                <Link href="/colleges" className="btn">View More</Link>
-              </div>
-            </div>
-          </div>
+          <CollegeCard key={college.id} college={college} />
         ))} />
+        <div className="d-flex justify-content-center pb-5">
+          <Link href='/colleges' className='btn viewMoreClgBtn'>Load More</Link>
+        </div>
       </div>
     </section>
   )
