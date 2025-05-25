ec3c20f2ce598438d8a5db0ebbc54d203c5d3d9d
abhishek <abhishekdahiya298@gmail.com>
2025-05-25 12:32:29 +0530

    less paylod

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,23 +1,23 @@
 import NextAuth from 'next-auth';
-import GoogleProvider from 'next-auth/providers/google';
-import LinkedInProvider from 'next-auth/providers/linkedin';
-import TwitterProvider from 'next-auth/providers/twitter';
+// import GoogleProvider from 'next-auth/providers/google';
+// import LinkedInProvider from 'next-auth/providers/linkedin';
+// import TwitterProvider from 'next-auth/providers/twitter';
 
 
 export default NextAuth({
   providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    }),
-    LinkedInProvider({
-      clientId: process.env.LINKEDIN_CLIENT_ID,
-      clientSecret: process.env.LINKEDIN_CLIENT_SECRET,
-    }),
-    TwitterProvider({
-      clientId: process.env.TWITTER_CLIENT_ID,
-      clientSecret: process.env.TWITTER_CLIENT_SECRET,
-    }),
+    // GoogleProvider({
+    //   clientId: process.env.GOOGLE_CLIENT_ID,
+    //   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    // }),
+    // LinkedInProvider({
+    //   clientId: process.env.LINKEDIN_CLIENT_ID,
+    //   clientSecret: process.env.LINKEDIN_CLIENT_SECRET,
+    // }),
+    // TwitterProvider({
+    //   clientId: process.env.TWITTER_CLIENT_ID,
+    //   clientSecret: process.env.TWITTER_CLIENT_SECRET,
+    // }),
     // FacebookProvider({
     //   clientId: process.env.FACEBOOK_CLIENT_ID,
     //   clientSecret: process.env.FACEBOOK_CLIENT_SECRET, // Fixed typo
