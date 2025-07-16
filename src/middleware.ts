737c5a83897ace747217e93fd5fe50978092d985
abhishek <abhishekdahiya298@gmail.com>
2025-07-16 11:52:08 +0530

    middelwarechanges

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,7 +4,7 @@ export async function middleware(request: NextRequest) {
   // const { pathname } = request.nextUrl
   const url = request.nextUrl.clone()
 
-  // console.log('Original pathname:', url)
+  console.log('Original pathname:', url)
 
   try {
     const apiUrl = `${process.env.NEXT_PUBLIC_API_URI}redirecturls`
@@ -13,22 +13,22 @@ export async function middleware(request: NextRequest) {
     if (response.ok) {
       const redirections = await response.json()
       const redirect = redirections.find((item: { old_url: string; new_url: string }) => item.old_url === url.pathname)
-      // console.log('=========== MIDDLEWARE ===========1111111111111', redirect)
+      console.log('1', redirect)
       if (redirect) {
-        // console.log('=========== MIDDLEWARE ===========222222222222222')
+        console.log('2')
         console.log('backend', redirect.new_url)
 
         url.pathname = redirect.new_url
         url.href = url.origin + redirect.new_url
+        console.log('3', url)
         return NextResponse.redirect(url, 301)
       }
       const shouldRedirect = !url.href.endsWith('/')
       if (shouldRedirect) {
         url.href = url.href + '/'
+        console.log('4', url)
         return NextResponse.redirect(url, 301)
       }
-
-
     } else {
       console.error(`Failed to fetch redirect mappings: ${response.statusText}`)
     }
