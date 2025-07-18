266ea612c1d1a8965662d4891e2840e06bbfd033
puneet k <puneet@svastir.com>
2025-07-18 18:15:23 +0530

    update

--- next.config.js
+++ next.config.js
@@ -3,11 +3,30 @@ const path = require('path')
 
 /** @type {import('next').NextConfig} */
 module.exports = {
-  //trailingSlash: true, // ← IMPORTANT
-  // skipTrailingSlashRedirect: true, // ← THIS DISABLES 308 REDIRECTS
+  trailingSlash: true, // ← IMPORTANT
+  //skipTrailingSlashRedirect: true, // ← THIS DISABLES 308 REDIRECTS
   images: {
     domains: ['api.learntechww.com', 'learntechww.com']
   },
+  redirects: async () => [
+    {
+      source: '/:path((?!_next|api).*)',
+      has: [
+        {
+          type: 'header',
+          key: 'x-vercel-id'
+        }
+      ],
+      missing: [
+        {
+          type: 'query',
+          key: '_nextDataReq'
+        }
+      ],
+      permanent: true, // 👈 this ensures 301 instead of 308
+      destination: '/:path/' // add trailing slash
+    }
+  ],
   swcMinify: true,
   experimental: {
     optimizeCss: true
