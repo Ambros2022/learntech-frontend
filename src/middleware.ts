098226ac36a98e7d5c92a4900f4205c9654873d2
abhishek <abhishekdahiya298@gmail.com>
2025-07-25 11:15:41 +0530

    thankyou removed

--- src/middleware.ts
+++ src/middleware.ts
@@ -10,7 +10,7 @@ export async function middleware(request: NextRequest) {
 
   // console.log("json=======================")
   if (pathname.endsWith('.json')) return NextResponse.next()
-    // console.log("json=======================2",pathname)
+    console.log("json=======================2",pathname)
   // ✅ Skip Next.js internal prefetch requests
   const isPrefetch = request.headers.get('x-middleware-prefetch') === '1'
   if (isPrefetch) return NextResponse.next()
@@ -54,6 +54,6 @@ export async function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
-    '/((?!_next/|api/|admin/login|app/dashboard(?:/.*)?|.*\\.(?:js|css|json|png|jpg|jpeg|gif|svg|ico|webp|woff2?|ttf|eot|map)).*)',
+    '/((?!_next/|api/|admin/login|thank-you|app/dashboard(?:/.*)?|.*\\.(?:js|css|json|png|jpg|jpeg|gif|svg|ico|webp|woff2?|ttf|eot|map)).*)',
   ],
 }
